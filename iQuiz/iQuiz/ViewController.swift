b05ed30ad0be2e867da1bc6cc79a8255662abd4e
thebrilliant <vivyanw@uw.edu>
2015-10-29 21:05:54 -0700

    blank app

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Vivyan Woods on 10/29/15.
+//  Copyright © 2015 Vivyan Woods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
