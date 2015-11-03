414200cbd0030e1ada85e21cbb99f187769a9d98
thebrilliant <vivyanw@uw.edu>
2015-11-03 12:43:04 -0800

    fixing app, got table to show up with text and pics

---
+++ iQuiz/iQuiz/SubjectController.swift
@@ -0,0 +1,9 @@
+//
+//  SubjectController.swift
+//  iQuiz
+//
+//  Created by Vivyan Woods on 11/3/15.
+//  Copyright © 2015 Vivyan Woods. All rights reserved.
+//
+
+import Foundation
--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
+    private let subjectsDescr = ["Math questions here!", "All the MSH trivia here!", "Science is so much fun!"]
+    let tableID = "QuizTopics"
+    
     @IBAction func settingsPressed(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Settings go here", message: "OK", preferredStyle: UIAlertControllerStyle.Alert)
         let action = UIAlertAction(title: "Okay", style: .Default, handler: nil)
@@ -26,7 +30,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjects.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(tableID) as UITableViewCell!
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: tableID)
+        }
+        cell.imageView?.image = UIImage(named: "200px-ALBW_Triforce")
+        cell.textLabel?.text = subjects[indexPath.row]
+        cell.detailTextLabel?.text = subjectsDescr[indexPath.row]
+        //cell.textLabel?.description = "TEST"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            let rowValue = subjects[indexPath.row]
+            let message = "You selected \(rowValue)"
+            
+            let controller = UIAlertController(title: "Row Selected",
+                message: message, preferredStyle: .Alert)
+            let action = UIAlertAction(title: "Yes I Did", style: .Default, handler: nil)
+            controller.addAction(action)
+            
+            presentViewController(controller, animated: true, completion: nil)
+    }
 }
 
