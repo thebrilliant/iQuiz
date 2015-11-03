924eab808e22f13b26e7f881af97f1d8790ad993
thebrilliant <vivyanw@uw.edu>
2015-11-03 15:19:55 -0800

    yean=h

--- iQuiz/iQuiz/SubjectController.swift
+++ iQuiz/iQuiz/SubjectController.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import UIKit
+
+class SubjectViewController: UIViewController {
+    //@IBOutlet weak var listQ: UILabel!
+    @IBOutlet weak var list: UITextView!
+    let mathQ = ["1+1?", "e is?"]
+    let superQ = ["Who is Peter Parker?", "Thor is from which mythology?"]
+    let scienceQ = ["What does antichiral mean?", "What is RNAse used for?"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        list.text = "\(mathQ[0]) \(mathQ[1]) \(superQ[0]) \(superQ[1]) \(scienceQ[0]) \(scienceQ[1])"
+        //listQ.text? = "\(mathQ[0]) \(superQ[0]) \(scienceQ[0])"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     private let subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
     private let subjectsDescr = ["Math questions here!", "All the MSH trivia here!", "Science is so much fun!"]
     let tableID = "QuizTopics"
+    private var sub : SubjectViewController!
     
     @IBAction func settingsPressed(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Settings go here", message: "OK", preferredStyle: UIAlertControllerStyle.Alert)
@@ -24,11 +25,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        subjectLoader()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func subjectLoader() {
+        if sub == nil {
+            sub = storyboard?.instantiateViewControllerWithIdentifier("Subject") as! SubjectViewController
+        }
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -38,25 +47,42 @@ class ViewController: UIViewController {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier(tableID) as UITableViewCell!
         if (cell == nil) {
-            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: tableID)
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: tableID)
         }
         cell.imageView?.image = UIImage(named: "200px-ALBW_Triforce")
         cell.textLabel?.text = subjects[indexPath.row]
         cell.detailTextLabel?.text = subjectsDescr[indexPath.row]
-        //cell.textLabel?.description = "TEST"
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-            let rowValue = subjects[indexPath.row]
-            let message = "You selected \(rowValue)"
+        let rowValue = subjects[indexPath.row]
+        /*
+        if sub != nil && sub.view.superview != nil {
             
-            let controller = UIAlertController(title: "Row Selected",
-                message: message, preferredStyle: .Alert)
-            let action = UIAlertAction(title: "Yes I Did", style: .Default, handler: nil)
-            controller.addAction(action)
-            
-            presentViewController(controller, animated: true, completion: nil)
+        }
+        */
+        switchViewController(nil, to: sub)
+        //let message = "You selected \(rowValue)"
+        //let controller = UIAlertController(title: "Row Selected", message: message, preferredStyle: .Alert)
+        //let action = UIAlertAction(title: "Yes I Did", style: .Default, handler: nil)
+        //controller.addAction(action)
+        
+        //presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    private func switchViewController(from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMoveToParentViewController(nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, atIndex: 0)
+            to!.didMoveToParentViewController(self)
+        }
     }
 }
 
